5376b8d5644b81eae3308d11dc32773506bfd860
Travis Casey <travis.w.casey@outlook.com>
2025-08-27 14:18:55 -0400

    Reformat license file header.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- */
- 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! The `chomp3rs` crate provides efficient and optimized algorithms for high-dimensional
 //! computation of homology. The project is currently in an early developmental state.
 
